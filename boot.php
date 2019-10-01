c323ff2a35fdc3c45e2566d1682870158bebb1a5
pschuchmann <ps@manetage.de>
2019-10-01 09:08:13 +0200

    Responsive Images
    Bilder sind im Backend ausgelaufen

--- boot.php
+++ boot.php
@@ -6,3 +6,7 @@
 if (rex::isBackend() && is_object(rex::getUser())) {
     rex_perm::register('project_manager[]');
 }
+
+if (rex::isBackend() && rex_be_controller::getCurrentPagePart(1) == 'project_manager') {
+    rex_view::addCssFile($this->getAssetsUrl('css/styles.css'));
+}
