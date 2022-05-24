42d2df3ab91f495c0261b2abff0901cf0cca2e03
rkemmere <rkemmereit@gmx.de>
2022-05-24 18:29:36 +0200

    Changes 1.4

--- plugins/server/update.php
+++ plugins/server/update.php
@@ -2,5 +2,6 @@
 if (rex_plugin::get('project_manager', 'server')->isInstalled()) {
   rex_sql_table::get(rex::getTable('project_manager_domain'))
   ->ensureColumn(new rex_sql_column('maintenance', 'tinyint', true))
+  ->ensureColumn(new rex_sql_column('param', 'text', true))
   ->alter();
 }
